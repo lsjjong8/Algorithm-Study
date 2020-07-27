b2b3292bb8086078fe0c8badf77d3d326d321460
Seung-jong Lee <52433499+lsjjong8@users.noreply.github.com>
2020-07-27 15:26:07 +0900

    Update BOJ1932.java

--- 0. WSAPT-Study/BOJ1932.java
+++ 0. WSAPT-Study/BOJ1932.java
@@ -39,12 +39,16 @@ public class Main {
 
             for (int i = 1; i < n; i++) {
                 dp[i] = new int[i*2];
-                for (int j = 0; j < i*2; j++) {
-                    if (count == 2) {
-                        count = 0;
+                for (int j = 0; j < i+1; j++) {
+                    if(j==0) {
+                        dp[i][j] = dp[i-1][j] + input[i][j];
+                    } else if(j == i) {
+                        dp[i][j] = dp[i-1][j-1] + input[i][j];
+                    } else {
+                        dp[i][j] = Math.max(dp[i-1][j] + input[i][j], dp[i-1][j-1] + input[i][j]);
                     }
-                    dp[i][j] = dp[i - 1][j / 2] + input[i][j / 2 + count];
-                    count++;
+
+//                    System.out.println(dp[i][j]);
 
                     if(i == n-1 && max < dp[i][j]) {
                         max = dp[i][j];
